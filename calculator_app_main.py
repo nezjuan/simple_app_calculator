157b3c32af8a732bcca82462567c7d2bb5728fca
nezjuan <juanmartinezat9@gmail.com>
2026-05-07 17:20:09 +0800

    added exception handling

--- calculator_app_main.py
+++ calculator_app_main.py
@@ -2,22 +2,31 @@ from calculator_functions import UserInput, CalculatorFunctions
 while True:
     print(" * Simple Calculator App * ")
 
-    user = UserInput()
-    user.ask_user(operator=None, num1=None, num2=None) 
+    try:
+        user = UserInput()
+        user.ask_user(operator=None, num1=None, num2=None) 
 
-    if user.operator == "add":
-        CalculatorFunctions.add(user)
+        if user.operator == "add":
+            CalculatorFunctions.add(user)
 
-    elif user.operator == "subtract":
-        CalculatorFunctions.subtract(user)
+        elif user.operator == "subtract":
+            CalculatorFunctions.subtract(user)
 
-    elif user.operator == "multiply":
-        CalculatorFunctions.multiply(user)
+        elif user.operator == "multiply":
+            CalculatorFunctions.multiply(user)
 
-    elif user.operator == "divide":
-        CalculatorFunctions.divide(user)
-    
-    choice=input("Operate more? (Y/N): ")
-    if choice.upper == "N":
-        print("Thank you for using the application!")
-        break
+        elif user.operator == "divide":
+            CalculatorFunctions.divide(user)
+
+    except ZeroDivisionError:
+        print("You Can't Divide by Zero 0!")
+    except ValueError:
+        print("Numbers Only Please!")
+    except Exception:
+        print("Something Went Wrong!")
+
+    while True:
+        choice=input("Operate more? (Y/N): ")
+        if choice.upper == "N":
+            print("Thank you for using the application!")
+            break
