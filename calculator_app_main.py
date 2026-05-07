8175ecdea89e3084d57a171892e1a2524af9f6c1
nezjuan <juanmartinezat9@gmail.com>
2026-05-07 16:19:41 +0800

    tried fixing it, forgot there were still errors on the function code file

--- calculator_app_main.py
+++ calculator_app_main.py
@@ -1,37 +1,20 @@
+from calculator_functions import UserInput, CalculatorFunctions
 while True:
     print(" * Simple Calculator App * ")
 
-    operator = input("What operation to use? (add, subtract, multiply, divide): ")
-    num1 = float(input("First Number: "))
-    num2 = float(input("Second Number: ")) 
+    UserInput() 
 
-    def add():
-        result= num1 + num2
-        print(f"Sum = {result}")
+    if UserInput.operator == "add":
+        CalculatorFunctions.add()
 
-    def subtract():
-        result = num1 - num2
-        print(f"Difference = {result}")
+    elif UserInput.operator == "subtract":
+        CalculatorFunctions.subtract()
 
-    def multiply():
-        result = num1 * num2
-        print(f"Product = {result}")
+    elif UserInput.operator == "multiply":
+        CalculatorFunctions.multiply()
 
-    def divide():
-        result = num1 / num2
-        print(f"Quotient = {result}")
-
-    if operator == "add":
-        add()
-
-    elif operator == "subtract":
-        subtract()
-
-    elif operator == "multiply":
-        multiply()
-
-    elif operator == "divide":
-        divide()
+    elif UserInput.operator == "divide":
+        CalculatorFunctions.divide()
     
     choice=input("Operate more? (Y/N): ")
     if choice != "N":
