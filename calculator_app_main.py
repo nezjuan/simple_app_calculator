120de1b08b3e2a4d87d594412f2d93fe9d5f143c
nezjuan <juanmartinezat9@gmail.com>
2026-05-07 16:33:08 +0800

    added user argument for the calculator functions in the main file, however loop mechanism needs fixing

--- calculator_app_main.py
+++ calculator_app_main.py
@@ -3,19 +3,19 @@ while True:
     print(" * Simple Calculator App * ")
 
     user = UserInput()
-    user.ask_user(Operator=None, Num1=None, Num2=None) 
+    user.ask_user(operator=None, num1=None, num2=None) 
 
     if user.operator == "add":
-        CalculatorFunctions.add()
+        CalculatorFunctions.add(user)
 
     elif user.operator == "subtract":
-        CalculatorFunctions.subtract()
+        CalculatorFunctions.subtract(user)
 
     elif user.operator == "multiply":
-        CalculatorFunctions.multiply()
+        CalculatorFunctions.multiply(user)
 
     elif user.operator == "divide":
-        CalculatorFunctions.divide()
+        CalculatorFunctions.divide(user)
     
     choice=input("Operate more? (Y/N): ")
     if choice != "N":
