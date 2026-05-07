4104c75f02f1648eb7bfc2f11a1b99b2870b7399
nezjuan <juanmartinezat9@gmail.com>
2026-05-07 18:42:52 +0800

    gave the program an ability to save the recent used operations in a txt file, needs debugging though

--- calculator_functions.py
+++ calculator_functions.py
@@ -15,19 +15,28 @@ class UserInput:
         self.num1 = float(input("First Number: "))
         self.num2 = float(input("Second Number: "))
 
-class CalculatorFunctions(UserInput):
+class SaveHistory(UserInput):
+    def save_history(self, result):
+        with open("history.txt", "a") as math_history:
+            math_history.write(f"{self.num1} {self.operator} {self.num2} = {result}\n")
+
+class CalculatorFunctions(UserInput, SaveHistory):
     def add(self):
         result= self.num1 + self.num2
         print(f"Sum: {self.num1} + {self.num2} = \033[91m{result}\033[0m\n")
+        self.save_history("add", self.num1, self.num2, result)
 
     def subtract(self):
         result = self.num1 - self.num2
         print(f"Difference: {self.num1} - {self.num2} = \033[91m{result}\033[0m\n")
+        self.save_history("subtract", self.num1, self.num2, result)
 
     def multiply(self):
         result = self.num1 * self.num2
         print(f"Product: {self.num1} x {self.num2} = \033[91m{result}\033[0m\n")
+        self.save_history("multiply", self.num1, self.num2, result)
 
     def divide(self):
         result = self.num1 / self.num2
         print(f"Quotient: {self.num1} / {self.num2} = \033[91m{result}\033[0m\n")
+        self.save_history("divide", self.num1, self.num2, result)
