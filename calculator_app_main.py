440ac6087c98fe5b3922edb3e50caeaeb023515d
nezjuan <juanmartinezat9@gmail.com>
2026-05-07 16:14:11 +0800

    now implementing OOP in the output, still needs some fixing

--- calculator_app_main.py
+++ calculator_app_main.py
@@ -1,44 +1,39 @@
-print(" * Simple Calculator App * ")
+while True:
+    print(" * Simple Calculator App * ")
 
-operator = input("What operation to use? (add, subtract, multiply, divide): ")
-num1 = float(input("First Number: "))
-num2 = float(input("Second Number: ")) 
+    operator = input("What operation to use? (add, subtract, multiply, divide): ")
+    num1 = float(input("First Number: "))
+    num2 = float(input("Second Number: ")) 
 
-def more():
-    while True:
-        choice=input("Operate more? (Y/N): ")
-        if choice != "N":
-            print("Thank you for using the application!")
-            break
+    def add():
+        result= num1 + num2
+        print(f"Sum = {result}")
 
-def add():
-    result= num1 + num2
-    print(f"Sum = {result}")
+    def subtract():
+        result = num1 - num2
+        print(f"Difference = {result}")
 
-def subtract():
-    result = num1 - num2
-    print(f"Difference = {result}")
+    def multiply():
+        result = num1 * num2
+        print(f"Product = {result}")
 
-def multiply():
-    result = num1 * num2
-    print(f"Product = {result}")
+    def divide():
+        result = num1 / num2
+        print(f"Quotient = {result}")
 
-def divide():
-    result = num1 / num2
-    print(f"Quotient = {result}")
+    if operator == "add":
+        add()
 
-if operator == "add":
-    add()
-    more()
+    elif operator == "subtract":
+        subtract()
 
-elif operator == "subtract":
-    subtract()
-    more()
+    elif operator == "multiply":
+        multiply()
 
-elif operator == "multiply":
-    multiply()
-    more()
-
-elif operator == "divide":
-    divide()
-    more()
+    elif operator == "divide":
+        divide()
+    
+    choice=input("Operate more? (Y/N): ")
+    if choice != "N":
+        print("Thank you for using the application!")
+        break
