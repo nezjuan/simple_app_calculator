bb74b98710b32ae3740cd44c1bdc4c09a2264c7f
nezjuan <juanmartinezat9@gmail.com>
2026-05-07 16:02:46 +0800

    made a looping mechanism, needs fixing though

--- calculator_app_main.py
+++ calculator_app_main.py
@@ -4,6 +4,13 @@ operator = input("What operation to use? (add, subtract, multiply, divide): ")
 num1 = float(input("First Number: "))
 num2 = float(input("Second Number: ")) 
 
+def more():
+    while True:
+        choice=input("Operate more? (Y/N): ")
+        if choice != "N":
+            print("Thank you for using the application!")
+            break
+
 def add():
     result= num1 + num2
     print(f"Sum = {result}")
@@ -22,12 +29,16 @@ def divide():
 
 if operator == "add":
     add()
+    more()
 
 elif operator == "subtract":
     subtract()
+    more()
 
 elif operator == "multiply":
     multiply()
+    more()
 
 elif operator == "divide":
     divide()
+    more()
